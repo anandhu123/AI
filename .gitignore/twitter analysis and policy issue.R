3ad4e0452a032b1cbff99f379bff11c3b5f0cab2
anandhu123 <31847910+anandhu123@users.noreply.github.com>
2018-02-28 11:05:44 +0530

    Update twitter analysis and policy issue.R

--- .gitignore/twitter analysis and policy issue.R
+++ .gitignore/twitter analysis and policy issue.R
@@ -6,10 +6,10 @@ library(RMySQL)
 library(dbConnect)
 
 # Set API Keys
-api_key <- "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-api_secret <- "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-access_token <- "xxxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxx"
-access_token_secret <- "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+api_key <- "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+api_secret <- "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+access_token <- "xxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxx"
+access_token_secret <- "xxxxxxxxxxxxxxxxxxxxxxxxxx"
 setup_twitter_oauth(api_key, api_secret, access_token, access_token_secret)
 
 #Extract latest tweets
@@ -22,101 +22,118 @@ myMorningTweets <- timeLine[these]
 
 #sql connection with R
 con = dbConnect(MySQL(), user ='root',password ='',dbname = 'my',host ='localhost')
+test="select * from register2 where status='not defined'"
+test<-dbGetQuery(con,test)
+test=nrow(test)
 
-#Reject with age limit
-age="select tweet,age from register2"
-agedb<-dbGetQuery(con,age)
-db=agedb[agedb$age >18,  ] 
-db<-data.frame(db)
-
-#Rejection confirmation
-ager=agedb[agedb$age <18,  ]
-ager<-data.frame(ager)
-rno=nrow(ager)
-for(m in 1:rno)
+if(test!=0)
 {
-  agereject=sprintf("update register2 set status='Rejected' where tweet= '%s'", ager$tweet[m])
-  dbGetQuery(con,agereject)
-}
-
-
-number<-nrow(db)
-for(k in 1:number)
-{
+  #Reject with age limit
+  age="select tweet,age from register2 where status= 'not defined'"
+  agedb<-dbGetQuery(con,age)
+  db=agedb[agedb$age >18,  ] 
+  db<-data.frame(db)
   
-  print(db[k,1])
-  #Process the retrived tweets
-  tweet=twListToDF(myMorningTweets)
-  tweets1=data.frame(tweet$screenName,tweet$text)
-  colnames(tweets1)=c("screenName","text")
-  tweets=tweets1[tweets1$screenName ==db[k,1],  ] 
-    
-  #Find the count of occurance
-  n=nrow(tweets)
-  if(n==0)
+  #Rejection confirmation
+  ager=agedb[agedb$age <18,  ]
+  ager<-data.frame(ager)
+  rno=nrow(ager)
+  if(rno!=0)
   {
-    change=sprintf("update register2 set status='Confirmed' where tweet= '%s'", db[k,1])
-    dbGetQuery(con,change) 
-    next
-  }
-  print("Count")
-  count=0
-  words=c("employee","job","job alert","occupation","job vacancy","vacancy","career","interview","placement")
-  num=length(words)
-
-  for(j in 1:num)
-  {
-   for(i in 1:n)
+    for(m in 1:rno)
     {
-       if(grepl(words[j], tweets[[i,2]])==TRUE)
-       {
+      agereject=sprintf("update register2 set status='Rejected' where tweet= '%s'", ager$tweet[m])
+      dbGetQuery(con,agereject)
+    }
+  
+  }  
+  
+  number<-nrow(db)
+  for(k in 1:number)
+  {
+    
+    print(db[k,1])
+    #Process the retrived tweets
+    tweet=twListToDF(myMorningTweets)
+    tweets1=data.frame(tweet$screenName,tweet$text)
+    colnames(tweets1)=c("screenName","text")
+    tweets=tweets1[tweets1$screenName ==db[k,1],  ] 
+    
+    #Find the count of occurance
+    n=nrow(tweets)
+    if(n==0)
+    {
+      change=sprintf("update register2 set status='Confirmed' where tweet= '%s'", db[k,1])
+      dbGetQuery(con,change) 
+      next
+    }
+    print("Count")
+    count=0
+    words=c("employee","job","job alert","occupation","job vacancy","vacancy","career","interview","placement")
+    num=length(words)
+    
+    for(j in 1:num)
+    {
+      for(i in 1:n)
+      {
+        if(grepl(words[j], tweets[[i,2]])==TRUE)
+        {
           count=count+1
-       }
+        }
+      }
+    }
+    print(count)
+    condition=db[k,1]
+    if(count>8)
+    {
+      change=sprintf("update register2 set status='Rejected' where tweet= '%s'", condition)
+      dbGetQuery(con,change)
+    }
+    else
+    {
+      change=sprintf("update register2 set status='Confirmed' where tweet= '%s'", condition)
+      dbGetQuery(con,change) 
     }
   }
-  print(count)
-  condition=db[k,1]
-  if(count>8)
+  
+  #Sent email confirmation with secret id
+  library(RDCOMClient)
+  search=sprintf("select * from register2 where status ='Confirmed' and mailconfirm='no'")
+  db=dbGetQuery(con,search)
+  
+  if(nrow(db)!=0){
+  mailc=sprintf("update register2 set mailconfirm='yes' where status ='Confirmed'")
+  dbGetQuery(con,mailc)
+  
+  for(i in 1:nrow(db))
   {
-    change=sprintf("update register2 set status='Rejected' where tweet= '%s'", condition)
-    dbGetQuery(con,change)
-  }
-  else
-  {
-    change=sprintf("update register2 set status='Confirmed' where tweet= '%s'", condition)
-    dbGetQuery(con,change) 
-  }
-}
-
-#Sent email confirmation with secret id
-library(RDCOMClient)
-search=sprintf("select * from register2 where status ='Confirmed'")
-db=dbGetQuery(con,search)
-for(i in 1:nrow(db))
-{
-  if(db[i,19]!="no gmail")
-  {
+    if(db[i,19]!="no gmail")
+    {
+      
+      body=sprintf("Hello %s, your application for medicare registration is accepted and your secret id for login is %d",db[i,1],db[i,12])
+      OutApp <- COMCreate("Outlook.Application")
+      outMail = OutApp$CreateItem(0)
+      outMail[["To"]] = db[i,19]
+      outMail[["subject"]] = "Medicare"
+      outMail[["body"]] = body
+      outMail$Send()
+    }
+    else
+    {
+      next
+    }
     
-     body=sprintf("Hello %s, your application for medicare registration is accepted and your secret id for login is %d",db[i,1],db[i,12])
-     OutApp <- COMCreate("Outlook.Application")
-     outMail = OutApp$CreateItem(0)
-     outMail[["To"]] = db[i,19]
-     outMail[["subject"]] = "Medicare"
-     outMail[["body"]] = body
-     outMail$Send()
-  }
-  else
-  {
-    next
   }
   
+  #Conditions applied
+  #1-age
+  #2-social media
+  #3-invalid twitter account or inactive twitter account=not defined
+}  
+  
+  
+}else
+{
+  print("No new registrations")
 }
 
-
-
-
-
-#Conditions applied
-#1-age
-#2-social media
-#3-invalid twitter account or inactive twitter account=not defined
