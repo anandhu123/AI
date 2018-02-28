6829f9613552748a43ec365e5dbfe65226eae801
anandhu123 <31847910+anandhu123@users.noreply.github.com>
2018-02-28 11:10:28 +0530

    Update claim.sql

--- .gitignore/claim.sql
+++ .gitignore/claim.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Feb 21, 2018 at 11:39 AM
+-- Generation Time: Feb 27, 2018 at 09:39 AM
 -- Server version: 5.1.53
 -- PHP Version: 5.3.4
 
@@ -35,23 +35,30 @@ CREATE TABLE IF NOT EXISTS `claim` (
   `number` int(22) NOT NULL,
   `duration` int(22) NOT NULL,
   `status` varchar(22) NOT NULL,
-  `reason` varchar(60) NOT NULL
+  `reason` varchar(60) NOT NULL,
+  `date` varchar(10) NOT NULL,
+  `smsstatus` varchar(22) NOT NULL DEFAULT 'no'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `claim`
 --
 
-INSERT INTO `claim` (`cid`, `Location`, `id`, `name`, `hospital`, `amount`, `number`, `duration`, `status`, `reason`) VALUES
-(123456, 'Palakkad', 24998, 'dental surgery', 'Fathima Hospital', 0, 0, 0, 'Rejected', 'Duplication'),
-(12131415, 'Idukki', 24998, 'hearing aids', 'S.N. Trust Medical Mission Hospital', 0, 0, 0, 'Rejected', 'Duplication'),
-(622605, 'Kochi', 24998, 'hearing aids', 'Aster', 100000, 0, 1000, 'Rejected', 'Duplication'),
-(475709, 'Kozhikode', 24998, 'dental surgery', 'MGM', 50000, 22, 12, 'Rejected', 'Duplication'),
-(1393617, 'Kollam', 24224, 'Fever', 'Aster', 1212, 22, 12, 'Rejected', 'Duplication'),
-(561327, 'Kollam', 24224, 'Fever', 'MGM', 1213, 20, 12, 'Rejected', 'Duplication'),
-(1598416, 'Kollam', 24224, 'Fever', 'MGM', 50000, 22, 52, 'Rejected', 'Duplication'),
-(1929652, 'Alappuzha', 63938, 'AIDS', 'Aster', 95846, 22, 2000, 'Rejected', 'Unreliable hospital network'),
-(1880385, 'Kottayam', 63938, 'burn', 'abcd', 50000, 22, 2000, 'Rejected', 'Unreliable hospital network'),
-(1857864, 'Kottayam', 63938, 'headache', 'abcd', 0, 22, 200, 'Rejected', 'Unreliable hospital network'),
-(829517, 'Kollam', 24224, 'Accident', 'pariyaram', 23625, 22, 12, 'Accepted', 'Eligible'),
-(1745249, 'Alappuzha', 19241, 'Japanese Encephalitis', 'T. D. Medical College, Alappuzha', 65487, 33, 45, 'Accepted', 'Eligible');
+INSERT INTO `claim` (`cid`, `Location`, `id`, `name`, `hospital`, `amount`, `number`, `duration`, `status`, `reason`, `date`, `smsstatus`) VALUES
+(123456, 'Palakkad', 24998, 'dental surgery', 'Fathima Hospital', 10002, 2, 3, 'Rejected', 'Duplication', '2018/02/22', 'yes'),
+(12131415, 'Idukki', 24998, 'hearing aids', 'S.N. Trust Medical Mission Hospital', 15423, 8, 2, 'Rejected', 'Duplication', '2018/02/21', 'yes'),
+(622605, 'Kochi', 24998, 'hearing aids', 'Aster', 100000, 21, 1000, 'Rejected', 'Duplication', '2018/02/20', 'yes'),
+(475709, 'Kozhikode', 24998, 'dental surgery', 'MGM', 50000, 22, 12, 'Rejected', 'Duplication', '2018/02/22', 'yes'),
+(1549106, 'Alappuzha', 72758, 'Stomach pain', 'T.D. Medical College Hospital Alappuzha', 3546, 5, 3, 'Accepted', 'Eligible', '2018/02/23', 'yes'),
+(1393617, 'Kollam', 24224, 'Fever', 'Aster', 1212, 22, 12, 'Rejected', 'Duplication', '2018/02/23', 'yes'),
+(561327, 'Kollam', 24224, 'Fever', 'MGM', 1213, 20, 12, 'Rejected', 'Duplication', '2018/02/23', 'yes'),
+(1598416, 'Kollam', 24224, 'Fever', 'MGM', 50000, 22, 52, 'Rejected', 'Duplication', '2018/02/23', 'yes'),
+(1929652, 'Alappuzha', 63938, 'AIDS', 'Aster', 95846, 22, 2000, 'Rejected', 'Excluded diseases', '2018/02/23', 'yes'),
+(1880385, 'Kottayam', 63938, 'burn', 'abcd', 50000, 22, 2000, 'Rejected', 'Unreliable hospital network', '2018/02/23', 'yes'),
+(1857864, 'Kottayam', 63938, 'headache', 'abcd', 23548, 22, 200, 'Rejected', 'Unreliable hospital network', '2018/02/23', 'yes'),
+(829517, 'Kollam', 24224, 'Accident', 'pariyaram', 23625, 22, 12, 'Accepted', 'Eligible', '2018/02/23', 'yes'),
+(1745249, 'Alappuzha', 19241, 'Japanese Encephalitis', 'T. D. Medical College, Alappuzha', 65487, 33, 45, 'Rejected', 'Unreliable hospital network', '2018/02/23', 'yes'),
+(1874691, 'Kottayam', 24998, 'Fever', 'aaaaa', 50000, 22, 3, 'Rejected', 'Unreliable hospital network', '2018/02/24', 'yes'),
+(1437159, 'Alappuzha', 34455, 'Cough', 'Sahrudaya Hospital', 4256, 5, 3, 'Accepted', 'Eligible', '26/02/2018', 'yes'),
+(1352546, 'Tirupati', 19241, 'Fever', 'Sri Venkateshwara Institute Of Medical Scien', 5489, 22, 12, 'Rejected', 'Duplication', '27/02/2018', 'yes'),
+(1896830, 'Tirupati', 19241, 'Fever', 'Sri Venkateshwara Institute Of Medical Scien', 5489, 22, 12, 'Rejected', 'Duplication', '27/02/2018', 'yes');
